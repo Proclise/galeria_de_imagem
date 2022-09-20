70dc0daf9280e291aef3b3f11cf7561bf1950606
Proclise <112097335+Proclise@users.noreply.github.com>
2022-09-20 17:58:49 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#botao_cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoNovaImagem = $('#endereco_imagem_nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+        <div class="overlay_imagem_link">
+        <a href="${enderecoNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+            Ver imagem em tamanho real
+        </a>
+        </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco_imagem_nova').val('');
+    })
+
+    })
+
